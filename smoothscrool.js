ce54a905f2fa65b94843161aeb007ac00baf41a2
Myhnea <77752326+Myhnea@users.noreply.github.com>
2021-08-26 12:51:35 +0300

    Add files via upload

---
+++ smoothscrool.js
@@ -0,0 +1,18 @@
+var tl = new TimelineMax({onUpdate:updatePercentage });
+    const controller = new ScrollMagic.Controller();
+
+    tl.from("h2", .5, {x: 200, opacity:0})
+
+    const scene = new ScrollMagic.Scene({
+        triggerElement: ".sticky",
+        triggerHook: "onLeave",
+        duration: "100%"
+    })
+    .setPin(".sticky")
+    .setTween(tl)
+    .addTo(controller);
+
+    function updatePercentage() {
+        tl.progress();
+        console.log(tl.progress());
+    }
